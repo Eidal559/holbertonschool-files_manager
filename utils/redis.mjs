991addef5af27859a84e0b2a4cd73a82f883ca58
Eidal559 <eidaljuarbe@gmail.com>
2024-11-20 19:20:47 -0400

    updated task

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,4 +1,4 @@
-import { createClient } from 'redis';
+const { createClient } = require('redis'); // CommonJS import
 
 class RedisClient {
   constructor() {
@@ -39,4 +39,4 @@ class RedisClient {
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+module.exports = redisClient; // Exporting using CommonJS
